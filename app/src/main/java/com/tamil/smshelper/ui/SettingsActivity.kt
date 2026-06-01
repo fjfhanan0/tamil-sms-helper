e57fad43f9cf130155783c96d5f202495d89649e
fjfhanan0 <183686810@qq.com>
2026-06-01 18:47:50 +0800

    Create SettingsActivity.kt

---
+++ app/src/main/java/com/tamil/smshelper/ui/SettingsActivity.kt
@@ -0,0 +1,11 @@
+package com.tamil.smshelper.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 简单占位，后续可用 PreferenceFragmentCompat 完善设置
+    }
+}
