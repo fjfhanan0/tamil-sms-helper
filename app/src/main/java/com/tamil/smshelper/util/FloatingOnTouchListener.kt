c6ac8be35562bbe43d7093c06f6dae164b099e69
fjfhanan0 <183686810@qq.com>
2026-06-01 18:49:01 +0800

    Create FloatingOnTouchListener.kt

---
+++ app/src/main/java/com/tamil/smshelper/util/FloatingOnTouchListener.kt
@@ -0,0 +1,34 @@
+package com.tamil.smshelper.util
+
+import android.view.MotionEvent
+import android.view.View
+import android.view.WindowManager
+
+class FloatingOnTouchListener(
+    private val params: WindowManager.LayoutParams,
+    private val windowManager: WindowManager
+) : View.OnTouchListener {
+    private var initialX = 0
+    private var initialY = 0
+    private var initialTouchX = 0f
+    private var initialTouchY = 0f
+
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                initialX = params.x
+                initialY = params.y
+                initialTouchX = event.rawX
+                initialTouchY = event.rawY
+                return true
+            }
+            MotionEvent.ACTION_MOVE -> {
+                params.x = initialX + (event.rawX - initialTouchX).toInt()
+                params.y = initialY + (event.rawY - initialTouchY).toInt()
+                windowManager.updateViewLayout(v, params)
+                return true
+            }
+        }
+        return false
+    }
+}
