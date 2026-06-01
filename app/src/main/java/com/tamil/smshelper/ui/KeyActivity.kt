6f6807c4d5a6a7d4acfdfe092d27e8fd0dbf2cae
fjfhanan0 <183686810@qq.com>
2026-06-01 18:48:15 +0800

    Create KeyActivity.kt

---
+++ app/src/main/java/com/tamil/smshelper/ui/KeyActivity.kt
@@ -0,0 +1,11 @@
+package com.tamil.smshelper.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class KeyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 备用密钥界面，目前未使用，保留以便扩展
+    }
+}
