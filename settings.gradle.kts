a67deed3729168f9c1828ce17aeaaebaac702770
fjfhanan0 <183686810@qq.com>
2026-06-02 19:37:45 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,13 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+dependencyResolution {
     repositories {
         google()
         mavenCentral()
     }
 }
+
 rootProject.name = "TamilSMSHelper"
 include(":app")
