5cfe33799af2af78bc6e94cf45fd50667ee96411
fjfhanan0 <183686810@qq.com>
2026-06-02 21:18:03 +0800

    Update MessageTemplateDao.kt

--- app/src/main/java/com/tamil/smshelper/data/MessageTemplateDao.kt
+++ app/src/main/java/com/tamil/smshelper/data/MessageTemplateDao.kt
@@ -4,18 +4,19 @@ import androidx.room.*
 
 @Dao
 interface MessageTemplateDao {
+
     @Query("SELECT * FROM message_templates ORDER BY id ASC")
-    suspend fun getAllTemplates(): List<MessageTemplate>
+    fun getAllTemplates(): List<MessageTemplate>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAll(templates: List<MessageTemplate>)
+    fun insertAll(templates: List<MessageTemplate>)
 
     @Update
-    suspend fun updateTemplate(template: MessageTemplate)
+    fun updateTemplate(template: MessageTemplate)
 
     @Query("UPDATE message_templates SET isSelected = 0")
-    suspend fun clearAllSelections()
+    fun clearAllSelections()
 
     @Query("SELECT * FROM message_templates WHERE isSelected = 1 LIMIT 1")
-    suspend fun getSelectedTemplate(): MessageTemplate?
+    fun getSelectedTemplate(): MessageTemplate?
 }
